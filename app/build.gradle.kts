1820fdf79db373a09a47d054db6122f7503caa6e
suryanshlabs <paidgamesofsp140@gmail.com>
2026-09-25 17:47:38 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,22 @@ android {
 
     defaultConfig {
         applicationId = "com.openspd"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
